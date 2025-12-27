5fd266400981b40cd975b9c83b45e44e837727c0
Vaibhav Satheesh <vaibhav.satheesh03@gmail.com>
2025-12-27 12:17:09 +0530

    Completed file of Filtering and selection.py

--- Assignments Python/Filtering and selection.py
+++ Assignments Python/Filtering and selection.py
@@ -16,3 +16,22 @@ print(south_orders)
 
 # Verify Count
 print("\nNumber of South region orders: ", len(south_orders))
+
+# ************************************************************************
+''' Management wants to see only Corporate customers.
+    Filter the customers DataFrame to show Corporate segment customers'''
+
+corporate_customers = customer[customer["segment"] == "Corporate"]
+print("\nCorporate Custumer: ")
+print(corporate_customers)
+
+# Verify Count
+print("\nNumber of Corporate customers:", len(corporate_customers))
+
+# ************************************************************************
+''' Identify all orders where quantity is greater than 10.
+    Display order_id customer_id and quantity'''
+
+high_quantity_orders = orders[orders["quantity"] > 10][["order_id", "customer_id", "quantity"]]
+print("\nOrders with quantity greater than 10: ")
+print(high_quantity_orders)
