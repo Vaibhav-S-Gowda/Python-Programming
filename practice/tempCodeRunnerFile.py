19e50eb823f8598d126e0a8c31deb659f40c2685
Vaibhav Satheesh <vaibhav.satheesh03@gmail.com>
2026-01-09 19:44:30 +0530

    tempcode delete

--- practice/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-except KeyboardInterrupt as e:
-#     print(f'{e} occured')
-# except KeyError as e:
-#     print(f'Key does not exist: {e}')
-# except IndexError as e:
-#     print('Index out of bound')
-# except ZeroDivisionError:
-#     print('Division by zero', ZeroDivisionError)
